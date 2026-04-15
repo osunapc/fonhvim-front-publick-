acda7703ae40f941bc0e22afe8acf21ea0bbaf47
osunapc <osunapc@gmail.com>
2026-04-14 22:15:15 -0400

    agregamos imagen en home

--- src/app/components/galeria-imagenes/galeria-imagenes.ts
+++ src/app/components/galeria-imagenes/galeria-imagenes.ts
@@ -13,7 +13,7 @@ export class GaleriaImagenes {
 
   // Placeholder images - Reemplazar con imágenes reales de assets
   images = [
-    'https://images.unsplash.com/photo-1560518883-ce09059eeffa?ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80',
+    '/edfon.webp',
     'https://images.unsplash.com/photo-1486406146926-c627a92ad1ab?ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80',
     'https://images.unsplash.com/photo-1497366216548-37526070297c?ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80',
   ];
